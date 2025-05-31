1a32f47bbb4f4dbee2cf0cef8966c5fb680e5736
GodProZEE <100044640+GodProZEE@users.noreply.github.com>
2025-05-31 16:38:18 +0500

    Allow saving and retrieval of multiple states
    to_json and from_json have been updated to allow for multiple
    saves, using the number of current elements in the lib/ directory.

--- lib/game_area.rb
+++ lib/game_area.rb
@@ -1,28 +1,36 @@
+require "json"
 class GameArea
-  attr_accessor :random_word, :blank_lines_for_word
+  attr_accessor :random_word, :blank_lines_for_word, :guesses_remaining, :blank_counter
 
-  def initialize(random_word)
+  def initialize(random_word, guesses_remaining, blank_lines_for_word, _blank_counter)
     @random_word = random_word
-    @blank_lines_for_word = new_display(random_word)
+    @guesses_remaining = guesses_remaining
+    @blank_lines_for_word = blank_lines_for_word
+    @blank_counter = blank_lines_for_word.count("_")
   end
 
-  def new_display(random_word)
+  def self.new_display(random_word)
     blank_lines_for_word = ""
     (0..random_word.size - 1).each do
       blank_lines_for_word.concat("_ ")
     end
+
     blank_lines_for_word
   end
 
   def make_guess
     puts "Please guess ONE letter"
-    gets.chomp
+
+    guess = gets.chomp
+    @guesses_remaining -= 1 if guess.size == 1
+    guess
   end
 
   def check_guess(word, guess_letter)
     word.include?(guess_letter)
   end
 
+  # Create an array to note the lcoatiosn of the letters (due to repititions) to update them later
   def locate_letters(word, guess_letter)
     array = []
     word.chars.each_with_index do |value, index|
@@ -35,15 +43,49 @@ class GameArea
     p blank_lines_for_word
   end
 
-  def update_display(blank_lines_for_word, word, guess_letter)
-    if check_guess(word, guess_letter)
+  def update_display(blank_lines_for_word, word, guess_letter, blank_counter)
+    if check_guess(word, guess_letter) && !blank_lines_for_word.include?(guess_letter)
 
       numbers_to_replace = locate_letters(word, guess_letter)
+
       numbers_to_replace.each do |value|
+        # value*2 because each blank is at index: index_of_character*2 due to spaces.
+
         blank_lines_for_word[value * 2] = guess_letter
       end
-    end
 
+      @blank_counter -= numbers_to_replace.size
+    end
+    p blank_counter
     display_blanks(blank_lines_for_word)
+  end
+
+  def reset_game
+    @random_word = random_word
+    @blank_lines_for_word = new_display(random_word)
+    @guesses_remaining = 7
+    @blank_counter = blank_lines_for_word.count("_")
+  end
+
+  def to_json(*_args)
+    fls = Dir.entries "lib/"
+
+    # There are 4 files intially, removal of 3 means file1 will be created. Add a new file
+    # and file2 will be created, and so on
+    number = fls.size - 3
+    file = File.new("lib/file#{number}.json", "w")
+    file.print JSON.dump({
+                           random_word: @random_word,
+                           blank_lines_for_word: @blank_lines_for_word,
+                           guesses_remaining: @guesses_remaining,
+                           blank_counter: @blank_counter
+                         })
+  end
+
+  def self.from_json(number)
+    file = File.open("lib/file#{number}.json", "r")
+    data = JSON.load file
+    p data
+    new(data["random_word"], data["guesses_remaining"], data["blank_lines_for_word"], data["blank_counter"])
   end
 end
--- main.rb
+++ main.rb
@@ -1,20 +1,56 @@
 require_relative "lib/dictionary"
 require_relative "lib/game_area"
+require "json"
 
-def create_dict_choose_word
-  dictionary = Dictionary.new("google-10000-english-no-swears.txt")
-  dictionary.choose_word
-end
+dictionary = Dictionary.new("google-10000-english-no-swears.txt")
 
-word = create_dict_choose_word
-game = GameArea.new(word)
-
-def looping_through_game(game)
+def looping_through_game(game, word)
   game.display_blanks(game.blank_lines_for_word)
-  loop do
+  puts "If at any point you wanna save, type 'save'"
+  p "Guesses remaining: #{game.guesses_remaining}, blank_counter: #{game.blank_counter}"
+  while !game.blank_counter.zero? && !game.guesses_remaining.zero?
     guess_letter = game.make_guess
-    game.update_display(game.blank_lines_for_word, game.random_word, guess_letter)
+    p "Guess letter: #{guess_letter}"
+    p guess_letter == "save"
+    if guess_letter == "save"
+      game.to_json
+      break
+    else
+      game.update_display(game.blank_lines_for_word, game.random_word, guess_letter, game.blank_counter)
+      p "Guesses remaining: #{game.guesses_remaining}, blank_counter: #{game.blank_counter}"
+    end
+    if game.guesses_remaining.zero?
+      puts "You kinda actually technically lost booooo"
+      break
+    elsif game.blank_counter.zero?
+      puts "Yayyy you won!"
+      break
+    end
   end
 end
 
-looping_through_game(game)
+def play_game(dictionary)
+  p "do you wanna play?"
+  answer = gets.chomp
+  return unless answer == "yes"
+
+  word = dictionary.choose_word
+  game = if ask_to_load_data == true
+           GameArea.from_json(choose_number)
+         else
+           GameArea.new(word, word.size + 10, GameArea.new_display(word), "")
+         end
+  looping_through_game(game, word)
+end
+
+def ask_to_load_data
+  puts "Do you wanna restore a save?"
+  answer = gets.chomp
+  answer.downcase == "yes"
+end
+
+def choose_number
+  puts "Please enter the number of the save you wanna restore"
+  gets.chomp.to_i
+end
+play_game(dictionary)
